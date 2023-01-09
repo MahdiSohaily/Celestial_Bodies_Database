2749450339a230a96455b2fb42e7144389dff69c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-01-09 22:18:22 +0430

    Add details table constraints

--- universe.sql
+++ universe.sql
@@ -56,3 +56,30 @@ CREATE TABLE public.details (
 
 
 ALTER TABLE public.details OWNER TO freecodecamp;
+
+--
+-- Name: details_details_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.details_details_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.details_details_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: details_details_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.details_details_id_seq OWNED BY public.details.details_id;
+
+
+--
+-- Name: galaxy; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
